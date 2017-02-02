6795dd2e7e379514aa30cd254bc55b90187dea0d
sigustin <simon.gustin@hotmail.com>
2017-02-02 16:53:28 +0100

    Add warning method

--- util/Log.java
+++ util/Log.java
@@ -4,6 +4,9 @@ public class Log {
    public static void log() {System.out.println("[LOG]");}
    public static void log(String msg) {System.out.println("[LOG] "+msg);}
 
+   public static void warn() {System.out.println("[WARNING]");}
+   public static void warn(String msg) {System.out.println("[WARNING] "+msg);}
+
    public static void err() {System.err.println("[ERR]");}
    public static void err(String msg) {System.err.println("[ERR] "+msg);}
 }
